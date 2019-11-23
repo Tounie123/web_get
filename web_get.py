5c2f67a6f811fadeb87ed7ff2c54f264ea212603
Tounie123 <740762622@qq.com>
2019-11-23 18:11:24 +0800

    complete web get

--- web_get.py
+++ web_get.py
@@ -1,9 +1,10 @@
-#!/bin/env python
+#!/usr/bin/env python
 # coding=utf-8  
 import requests
 import re
 import chardet
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
 
 heads = {}
 heads['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0'
@@ -21,26 +22,25 @@ def get_one_page(num):
     print(url)
     response = requests.get(url,headers=heads)
     soup = BeautifulSoup(response.content, "html.parser")
-    block = soup.find_all(class_="article-list leftpic")
-    for item in block:
-        data = item.find(class_="title")
-        print(data)
-        href = data.get("href")
-        print(href)
-    '''
-    data = []
-    for item in block:
-        print(item)
-        data.append(item.get('href'))
-    print(data)
-    '''
+    block = soup.find_all('span',class_="title")
+    info_lists = []
+    for data in block:
+        href = data.a.get('href')
+        title = data.get_text()
+        info_lists.append([title,href])
+    return info_lists
 
 
 def main():
+    wb = Workbook()
+    ws = wb.get_active_sheet()
+    ws.append(['title','link'])
     all_pages = get_last_page()
-    #for i in range(1,int(all_pages)+1):
-    #    get_one_page(i)
-    get_one_page(43)
+    for i in range(1,int(all_pages)+1):
+        info_lists = get_one_page(i)
+        for info_list in info_lists:
+            ws.append(info_list)
+    wb.save('linux china.xlsx')
 
 
 if __name__ == '__main__':
